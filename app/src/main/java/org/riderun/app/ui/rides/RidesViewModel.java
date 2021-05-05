d63a740307bcf35d476086e04c43c301ef28feb8
Christian Esken <chris.svc@arcor.de>
2021-05-05 23:16:25 +0200

    Choose initial park on the Ride View by locale of the user

--- app/src/main/java/org/riderun/app/ui/rides/RidesViewModel.java
+++ app/src/main/java/org/riderun/app/ui/rides/RidesViewModel.java
@@ -1,9 +1,11 @@
 package org.riderun.app.ui.rides;
 
+import org.riderun.app.model.City;
 import org.riderun.app.model.Count;
 import org.riderun.app.model.Park;
 import org.riderun.app.model.Ride;
 import org.riderun.app.provider.ProviderFactory;
+import org.riderun.app.provider.city.CityProvider;
 import org.riderun.app.provider.count.CountProvider;
 import org.riderun.app.provider.count.db.PoiKey;
 import org.riderun.app.provider.park.ParksProvider;
@@ -11,6 +13,7 @@ import org.riderun.app.provider.ride.RidesProvider;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import androidx.lifecycle.LiveData;
@@ -25,18 +28,16 @@ public class RidesViewModel extends ViewModel {
     private final MutableLiveData<RidesData> data = new MutableLiveData<>();
     // The following fields are used to update
     private final CountProvider countProvider = ProviderFactory.countProvider();
-    private final ParksProvider parks = ProviderFactory.parksProvider();
+    private final ParksProvider parksProvider = ProviderFactory.parksProvider();
+    private final CityProvider cityProvider = ProviderFactory.cityProvider();
     private final RidesProvider ridesProvider = ProviderFactory.ridesProvider();
-    List<Ride> rides;
-    //Map<PoiKey, Count> counts;
 
     public RidesViewModel() {
-        Park park = parks.all().get(0);
-        rides = ridesProvider.ridesForPark(park.getRcdbId());
-        RidesData rdata = new RidesData(park, rides, reloadCounts());
+        Park park = chooseInitialPark();
+        List<Ride> rides = park == null ? null : ridesProvider.ridesForPark(park.getRcdbId());
+        RidesData rdata = new RidesData(park, rides, loadCounts(rides));
         data.setValue(rdata);
     }
-
 
     /**
      * Informs that fields in the park or rides has changed
@@ -47,8 +48,14 @@ public class RidesViewModel extends ViewModel {
         data.postValue(existingValue);
     }
 
-
-    private Map<PoiKey, Count> reloadCounts() {
+    /**
+     * Loads the counts for the givne rides from the Count Provider (usually the SQLite of the app).
+     * All known counts will be returned.
+     *
+     * @param rides The rides
+     * @return the Counts
+     */
+    private Map<PoiKey, Count> loadCounts(List<Ride> rides) {
         Map<PoiKey, Count> counts = new HashMap<>();
         for (Ride ride : rides) {
             counts.putAll(countProvider.getByPoiKey(Integer.toString(ride.rcdbId())));
@@ -76,6 +83,50 @@ public class RidesViewModel extends ViewModel {
         // as counts for ALL rides of this Model are reloaded. Usually this is only a dozend or less,
         // but if the user would be able to show all rides (10000 in the whole world), this would be
         // not so efficient. For now we don't do this premature optimization.
-        data.postValue(new RidesData(existingValue.park, existingValue.rides, reloadCounts()));
+        data.postValue(new RidesData(existingValue.park, existingValue.rides, loadCounts(existingValue.rides)));
+    }
+
+
+    private Park chooseInitialPark() {
+        // We should pick the last park that the user had selected (taken from the DB).
+        // Alternatively - for the first run - select the closest park or the biggest pork in the
+        // country of the user.
+        String cc2letter = Locale.getDefault().getCountry();
+        Park park = chooseInitialPark(cc2letter);
+        if (park == null) {
+            // A user in a locale w/o parks will see something else. For now we pick "DE"
+            park = chooseInitialPark("GB");
+        }
+        if (park == null) {
+            // Still noting? Just pick the first park (if any)
+            List<Park> allParks = parksProvider.all();
+            park = allParks.isEmpty() ? null : allParks.get(0);
+        }
+
+        return park;
+    }
+
+    private Park chooseInitialPark(String cc2letter) {
+
+        Park bestPark = null;
+        int bestParkRideCount = 0;
+
+        List<Park> allParks = parksProvider.all();
+        if (allParks.isEmpty()) {
+            return null;
+        }
+
+        for (Park park : allParks) {
+            City city = cityProvider.byCityId(park.getCityId(), false);
+            if (city != null && cc2letter.equals(city.getCountry2letter())) {
+                List<Ride> rides = ridesProvider.ridesForPark(park.getRcdbId());
+                int rideCount = rides.size();
+                if (bestPark == null || rideCount > bestParkRideCount) {
+                    bestPark = park;
+                    bestParkRideCount = rideCount;
+                }
+            }
+        }
+        return bestPark;
     }
 }
